75f08f2726793c9fd02cb57b46ed2387c12ab8db
jvictornascimento <victojoao789@gmail.com>
2025-02-23 21:46:20 -0300

    Project hr-api-gateway created

---
+++ hr-api-gateway/src/test/java/com/jvictornascimento/hrapigateway/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
